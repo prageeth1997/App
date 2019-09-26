aad38b9cc78c0216dcf59b3c6d70f09d21906f7c
prageeth2 <prageeth.456>
2019-09-27 01:12:07 +0530

    commit by prageeth (MyOrder get values)

--- app/src/main/java/com/example/mobileshop/myorders.java
+++ app/src/main/java/com/example/mobileshop/myorders.java
@@ -3,6 +3,10 @@ package com.example.mobileshop;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +16,39 @@ public class myorders extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.myorders);
+
+
+
+        LinearLayout layout = (LinearLayout) findViewById(R.id.mainLayout);
+
+
+        for (int i = 0; i < 3; i++) {
+            LinearLayout row = new LinearLayout(this);
+            row.setOrientation(LinearLayout.VERTICAL);
+            row.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            row.setBackgroundResource(R.drawable.order_outline);
+
+                Button btnTag1;
+                btnTag1 = new Button(this);
+                btnTag1.setId(i);
+                btnTag1.setText(""+i);
+
+           // btnTag1.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT , 10));
+                //btnTag1.setBackgroundResource(R.drawable.radiustext);
+                //btnTag1.setWidth();
+                row.addView(btnTag1);
+
+               /* Button btnTag2;
+                btnTag2 = new Button(this);
+                btnTag2.setId(i);
+                btnTag2.setText(""+i);
+                //  btnTag2.setBackgroundResource(R.drawable.radiustext2);
+                row.addView(btnTag2);*/
+
+
+
+            layout.addView(row);
+        }
     }
 
     public void refund(View view) {
--- app/src/main/java/com/example/mobileshop/refundUpdate.java
+++ app/src/main/java/com/example/mobileshop/refundUpdate.java
@@ -3,17 +3,98 @@ package com.example.mobileshop;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class refundUpdate extends AppCompatActivity {
 
+    EditText txt1 , txt2 , txt3 , txt4;
+    TextView textV1 , textV2,textV3 , textV4,textV5 , textV6;
+    RadioButton rad1 , rad2;
+    ProgressBar proBar;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_refund_update);
+
+        txt1 = findViewById(R.id.editText1);
+        txt2 = findViewById(R.id.editText2);
+        txt3 = findViewById(R.id.amount1);
+        txt4 = findViewById(R.id.editText4);
+        textV1 = findViewById(R.id.textView16);
+        textV2 = findViewById(R.id.textView18);
+
+        textV1 = findViewById(R.id.textView1);
+        textV2 = findViewById(R.id.textView2);
+        textV3 = findViewById(R.id.textView3);
+        textV4 = findViewById(R.id.textView4);
+        textV5 = findViewById(R.id.textView17);
+        textV6 = findViewById(R.id.textView21);
+
+        rad1 = findViewById(R.id.radioButton1);
+        rad2 = findViewById(R.id.radioButton2);
+        proBar = findViewById(R.id.progress);
+        btn = findViewById(R.id.button4);
+
+        Intent intent = getIntent();
+        textV2.setText(intent.getStringExtra("ID"));
+
+        DatabaseReference readrefUpdate = FirebaseDatabase.getInstance().getReference().child("RefundDB").child(textV2.getText().toString());
+        readrefUpdate.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.hasChildren()){
+                    txt1.setText(dataSnapshot.child("reason").getValue().toString());
+                    txt2.setText(dataSnapshot.child("description").getValue().toString());
+                    String radio = dataSnapshot.child("type").getValue().toString();
+                    if(radio.equals("Half")) {
+                        rad2.setChecked(true);
+                        rad1.setChecked(false);
+                        txt3.setVisibility(View.VISIBLE);
+                        txt3.setText(dataSnapshot.child("amount").getValue().toString());
+
+                    }else {
+                        rad1.setChecked(true);
+                        rad2.setChecked(false);
+                    }
+                    textV1.setVisibility(View.VISIBLE);
+                    textV2.setVisibility(View.VISIBLE);
+                    textV3.setVisibility(View.VISIBLE);
+                    textV4.setVisibility(View.VISIBLE);
+                    textV5.setVisibility(View.VISIBLE);
+                    textV6.setVisibility(View.VISIBLE);
+
+
+                    txt1.setVisibility(View.VISIBLE);
+                    txt2.setVisibility(View.VISIBLE);
+                    txt3.setVisibility(View.VISIBLE);
+                    txt4.setVisibility(View.VISIBLE);
+                    rad1.setVisibility(View.VISIBLE);
+                    rad2.setVisibility(View.VISIBLE);
+                    btn.setVisibility(View.VISIBLE);
+                    proBar.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
     public void fully(View view) {
